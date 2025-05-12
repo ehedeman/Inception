a84991612576561f7cb9b53c19f9e85344569b2c
root <root@INSCRYPTION.ehedeman.42.fr>
2025-05-12 07:50:09 -0500

    merge

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out $CERTS_ -subj "/C=DE/L=WOB/O=1337/OU=student/CN=ehedeman.42.de"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out $CERTS_ -subj "/C=DE/L=WOB/O=1337/OU=student/CN=ehedeman.42.fr"
 
 
 echo "
