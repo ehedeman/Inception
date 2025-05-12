e8c1d51f241b5c161f13d5537a2ba1696659abf7
root <root@INSCRYPTION.ehedeman.42.fr>
2025-05-12 10:26:05 -0500

    dfljs

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -3,7 +3,7 @@ mkdir -p /var/run/mysqld
 chown -R mysql:mysql /var/run/mysqld
 
 service mysql start
-sleep 5
+#sleep 5
 
 echo "CREATE DATABASE IF NOT EXISTS $db_name ;" > db1.sql
 echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;" >> db1.sql
