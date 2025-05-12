3dd08160533c24b59d1a3be8dda688b132f4fe12
root <root@INSCRYPTION.ehedeman.42.fr>
2025-05-12 08:29:50 -0500

    commit

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out $CERTS_ -subj "/C=DE/L=WOB/O=1337/OU=student/CN=ehedeman.42.fr"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out $CERTS_ -subj "/C=DE/L=WOB/O=42/OU=student/CN=ehedeman.42.fr"
 
 
 echo "
@@ -8,7 +8,7 @@ server {
     listen 443 ssl;
     listen [::]:443 ssl;
 
-    #server_name www.$DOMAIN_NAME $DOMAIN_NAME;
+    server_name www.$DOMAIN_NAME $DOMAIN_NAME;
 
     ssl_certificate $CERTS_;
     ssl_certificate_key /etc/ssl/private/nginx-selfsigned.key;" > /etc/nginx/sites-available/default
@@ -21,9 +21,9 @@ echo '
     root /var/www/html;
 
     location ~ [^/]\.php(/|$) { 
-            try_files $uri =404;
-            fastcgi_pass wordpress:9000;
             include fastcgi_params;
+            #try_files $uri =404;
+            fastcgi_pass wordpress:9000;
             fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
         }
 } ' >>  /etc/nginx/sites-available/default
