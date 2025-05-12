eee3b004c01d24966c90811f8e920252c78435a5
root <root@INSCRYPTION.ehedeman.42.fr>
2025-05-12 10:12:27 -0500

    debug

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,6 +1,10 @@
 #!bin/bash
+mkdir -p /var/run/mysqld
+chown -R mysql:mysql /var/run/mysqld
+
 service mysql start
 sleep 5
+
 echo "CREATE DATABASE IF NOT EXISTS $db_name ;" > db1.sql
 echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;" >> db1.sql
 echo "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;" >> db1.sql
