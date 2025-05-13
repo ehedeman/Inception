58724538e5e8066e57280afac29bfedf84b8bf50
root <root@INSCRYPTION.ehedeman.42.fr>
2025-05-13 03:35:02 -0500

    dsdfj

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -8,7 +8,7 @@ service mysql start
 echo "CREATE DATABASE IF NOT EXISTS $db_name ;" > db1.sql
 echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;" >> db1.sql
 echo "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED VIA mysql_native_password USING '1234' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 
 mysql < db1.sql
