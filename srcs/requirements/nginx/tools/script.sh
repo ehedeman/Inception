f5d5c07bec231f666a56908e8e72d2cf4c93e4b3
Marlon Hedemann <ehedeman@c2r2s6.42wolfsburg.de>
2025-05-21 13:09:05 +0200

    nginx up and running to subject standarts, rest is still not done

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,5 +1,7 @@
 #!bin/bash
 
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/ehedeman.key -out /etc/ssl/certs/ehedeman.crt -subj="/C=DE/ST=Niedersachsen/L=Woflsburg/O=42Wob/OU=ehedeman/CN=ehedeman.42.fr"
+
 echo "test"
 
 nginx -g "daemon off;"
